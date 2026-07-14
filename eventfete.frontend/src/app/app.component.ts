a390210b4b7802be614c2fcc3f0f46676fb5638d
jemaakourda <jemaa.kourda@gmail.com>
2026-07-14 11:12:00 +0100

    Ajout du footer avec copyright Jemaa Kourda 2026

--- eventfete.frontend/src/app/app.component.ts
+++ eventfete.frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar.component';
+import { FooterComponent } from './shared/components/footer.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
-  template: `<app-navbar></app-navbar><router-outlet></router-outlet>`,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
+  template: `<app-navbar></app-navbar><router-outlet></router-outlet><app-footer></app-footer>`,
   styles: []
 })
 export class AppComponent {}
